30d32881ad4452abea90af3a5fd770fbb386bc1a
rafaelolaru <olarurafael1801@gmail.com>
2021-11-29 21:03:15 +0200

    renewed

--- olarurafael.py
+++ olarurafael.py
@@ -4,7 +4,7 @@ headers = {
     'Accept': 'application/vnd.github.v3+json',
 }
 
-response = requests.get('https://api.github.com/users/mateasmario', headers=headers)
+response = requests.get('https://api.github.com/orgs/dsc-upt/repos', headers=headers)
 responseJson = response.json()
 
 print(responseJson[0]['name'])
