5b3757a348edb55d171f8c8939450e47783c71c9
Dani Munteanu <dani.munteanu.dm@gmail.com>
2021-11-29 20:54:39 +0200

    adaugat request
    un exemplu

---
+++ MunteanuDaniel.py
@@ -0,0 +1,10 @@
+import requests
+
+headers = {
+    'Accept': 'application/vnd.github.v3+json',
+}
+
+response = requests.get('https://api.github.com/repos/octocat/hello-world', headers=headers)
+responseJson = response.json()
+
+print(responseJson['forks'])
