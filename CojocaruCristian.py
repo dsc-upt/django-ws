50f13a8f09101784a4e6942d7e1ae171de6ccf87
Cojocaru Cristian <cojocaru.cristian20@gamil.com>
2021-11-29 21:01:08 +0200

    new

--- CojocaruCristian.py
+++ CojocaruCristian.py
@@ -5,6 +5,6 @@ headers = {
 }
 
 response = requests.get('https://api.github.com/repos/octocat/hello-world/languages', headers=headers)
-response.json = response.json(headers)
+responseJson = response.json(headers)
 print(response)
-print(response.json)
+print(responseJson ['name'])
